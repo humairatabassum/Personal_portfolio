3ee6d776d2dd23f88276ebaf1878d473d4191ec3
Humaira Tabassum <humaira.tabassumkhan@gmail.com>
2024-06-02 18:32:48 +0600

    #

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -7,15 +7,21 @@ import Image from "next/image"
 export const Photo = () => {
     return (
         <div className="w-full h-fyll relative">
-            <motion.div>
-                <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
+            <motion.div initial={{opacity:0}}
+                animate={{ opacity: 1, transition: { delay: 2, duration: 0.4, ease: "easeIn"}}}>
+                
+                
+                <motion.div initial={{ opacity: 0 }}
+                    animate={{ opacity: 0, transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" } }} className="w-[298px] h-[298px] 
+                xl:w-[498px] xl:h-[498px] mix-blend-screen">
+                    
                     <Image src="/assets/resume/imgDp1.png"
                         priority quality={100}
                         fill
                         alt=""
                         className="object-contain"
                     />
-                </div>
+                </motion.div>
             </motion.div>
         </div>
     )
