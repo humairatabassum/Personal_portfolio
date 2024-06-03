f89a155d4ef802c7ac1920e3b9ffc8260b5873d9
Humaira Tabassum <humaira.tabassumkhan@gmail.com>
2024-06-03 18:15:27 +0600

    #

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div className='container mx-auto h-full'>
       <div className='flex flex-col lg:flex-row items-center justify-between lg:pt-8 lg:pb-24'>
 
-        <div className="text-center lg:text-left">
+        <div className="text-center lg:text-left order-2 lg:order-none">
           <span className='text-xl'>Lorem Heading</span>
           <h1 className='h1 mb-6'>Hello I'm<br />
             <span className="text-purple-500">Lorem ipsum </span>
@@ -38,7 +38,7 @@ const Home = () => {
 
         </div>
 
-        <div>
+        <div className='order-1 lg:order-none'>
           <Photo/>
         </div>
       </div>
