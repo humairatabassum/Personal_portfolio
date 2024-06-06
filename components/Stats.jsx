dffc7b08b8c3bed6741ef5f7ddd6e63f080c862a
Humaira Tabassum <humaira.tabassumkhan@gmail.com>
2024-06-06 23:27:39 +0600

    #

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -22,7 +22,8 @@ const stats = [
 ]
 
 const Stats = () => {
-  return <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+  return(
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
     <div className="Container mx-auto">
       <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
         {stats.map((item, index) => {
@@ -42,6 +43,9 @@ const Stats = () => {
       </div>
     </div>
   </section>
+  
+  )
+  
 }
 
 export default Stats
